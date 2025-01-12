78d2f16486f84484140c78c86223cd802699951f
sainadh147 <sainadhbalam@gmail.com>
2025-01-13 00:23:25 +0530

    Add files to the project

---
+++ src/config.js
@@ -0,0 +1,19 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXv7B5U0Ma5w1RhmxfREP1fyOr7rqQIi4",
+  authDomain: "movie-trialers-hub.firebaseapp.com",
+  projectId: "movie-trialers-hub",
+  storageBucket: "movie-trialers-hub.firebasestorage.app",
+  messagingSenderId: "208582071649",
+  appId: "1:208582071649:web:a3143873986ac6311f2120"
+};
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmM0MDkxMDhhOGY5ZGRmNGRkOGQyZTRlYWI2NDMzYiIsIm5iZiI6MTczNjY5NTkxMy44NzksInN1YiI6IjY3ODNlMDY5NjAxYWNmZTdiZDRmMWU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4u7HM-z2_rnpCcB0e3draDmYUmF3Pu23-9viSUer8J8";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
